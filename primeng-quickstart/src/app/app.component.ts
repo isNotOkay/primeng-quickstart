7639de2dc1122050196b9b780578195e89db1138
kayod <kay.odenthal@gmx.de>
2025-10-16 09:57:48 +0200

    adjust layout

--- primeng-quickstart/src/app/app.component.ts
+++ primeng-quickstart/src/app/app.component.ts
@@ -1,40 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 import { TableModule } from 'primeng/table';
+import { SplitterModule } from 'primeng/splitter';
 import { DecimalPipe } from '@angular/common';
 import { ProductService } from './service/productsservice';
-
-export interface Product {
-  id?: string;
-  code?: string;
-  name?: string;
-  description?: string;
-  price?: number;
-  quantity?: number;
-  inventoryStatus?: string;
-  category?: string;
-  image?: string;
-  rating?: number;
-}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TableModule, DecimalPipe],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  imports: [TableModule, SplitterModule, DecimalPipe],
+  templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
-  products: Product[] = [];
+  products: any[] = [];
   rowsPerPageOptions = [5, 10, 20, 50];
 
   constructor(private productService: ProductService) {}
-
-  ngOnInit() {
-    // use full dataset so pagination makes sense
-    this.productService.getProducts().then(data => (this.products = data));
-  }
-
-  trackById(index: number, item: Product) {
-    return item.id ?? index;
-  }
+  ngOnInit() { this.productService.getProducts().then(d => this.products = d); }
+  rowTrackBy(i: number, p: any) { return p.id ?? p.code ?? i; }
 }
