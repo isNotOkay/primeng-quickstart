a8b9f974ecb7b4b74881aa3c06e20a75580535ee
kayod <kay.odenthal@gmx.de>
2025-10-16 10:11:09 +0200

    adjust paginator

--- primeng-quickstart/src/app/app.component.ts
+++ primeng-quickstart/src/app/app.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { TableModule } from 'primeng/table';
-import { SplitterModule } from 'primeng/splitter';
-import { DecimalPipe } from '@angular/common';
-import { ProductService } from './service/productsservice';
+import {Component, OnInit} from '@angular/core';
+import {TableModule} from 'primeng/table';
+import {SplitterModule} from 'primeng/splitter';
+import {DecimalPipe} from '@angular/common';
+import {ProductService} from './service/productsservice';
 
 @Component({
   selector: 'app-root',
@@ -12,9 +12,15 @@ import { ProductService } from './service/productsservice';
 })
 export class AppComponent implements OnInit {
   products: any[] = [];
-  rowsPerPageOptions = [5, 10, 20, 50];
 
-  constructor(private productService: ProductService) {}
-  ngOnInit() { this.productService.getProducts().then(d => this.products = d); }
-  rowTrackBy(i: number, p: any) { return p.id ?? p.code ?? i; }
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit() {
+    this.productService.getProducts().then(d => this.products = d);
+  }
+
+  rowTrackBy(i: number, p: any) {
+    return p.id ?? p.code ?? i;
+  }
 }
