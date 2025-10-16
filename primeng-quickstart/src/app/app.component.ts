baf52a8133dabaf6ffff92c136029b19d77fd363
kayod <kay.odenthal@gmx.de>
2025-10-16 10:25:11 +0200

    add select

--- primeng-quickstart/src/app/app.component.ts
+++ primeng-quickstart/src/app/app.component.ts
@@ -1,25 +1,43 @@
-import {Component, OnInit} from '@angular/core';
-import {TableModule} from 'primeng/table';
-import {SplitterModule} from 'primeng/splitter';
-import {DecimalPipe} from '@angular/common';
-import {ProductService} from './service/productsservice';
-import {Toolbar} from 'primeng/toolbar';
-import {ButtonDirective} from 'primeng/button';
+// file: src/app/app.component.ts
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { SplitterModule } from 'primeng/splitter';
+import { DecimalPipe } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { SelectModule } from 'primeng/select';
+import { ProductService } from './service/productsservice';
+import { Toolbar } from 'primeng/toolbar';
+import { ButtonDirective } from 'primeng/button';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [TableModule, SplitterModule, DecimalPipe, Toolbar, ButtonDirective],
+  imports: [
+    TableModule,
+    SplitterModule,
+    DecimalPipe,
+    FormsModule,
+    SelectModule,
+    Toolbar,
+    ButtonDirective
+  ],
   templateUrl: './app.component.html'
 })
 export class AppComponent implements OnInit {
   products: any[] = [];
 
-  constructor(private productService: ProductService) {
-  }
+  // --- Added for Select ---
+  dataSources = [
+    { label: 'SQLite', value: 'sqlite' },
+    { label: 'Excel', value: 'excel' }
+  ];
+  selectedDataSource: string = 'all';
+  // ------------------------
+
+  constructor(private productService: ProductService) {}
 
   ngOnInit() {
-    this.productService.getProducts().then(d => this.products = d);
+    this.productService.getProducts().then(d => (this.products = d));
   }
 
   rowTrackBy(i: number, p: any) {
