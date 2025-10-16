287d24777e8b7708b5d49ac9c51977e9c80b337b
kayod <kay.odenthal@gmx.de>
2025-10-16 16:10:36 +0200

    remove obsolete code

--- primeng-quickstart/src/app/app.config.ts
+++ primeng-quickstart/src/app/app.config.ts
@@ -1,8 +1,6 @@
-import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
-import { routes } from './app.routes';
-import { providePrimeNG } from 'primeng/config';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import {ApplicationConfig, provideZoneChangeDetection} from '@angular/core';
+import {providePrimeNG} from 'primeng/config';
+import {provideAnimationsAsync} from '@angular/platform-browser/animations/async';
 import Aura from '@primeuix/themes/aura';
 
 export const appConfig: ApplicationConfig = {
@@ -11,10 +9,9 @@ export const appConfig: ApplicationConfig = {
     providePrimeNG({
       theme: {
         preset: Aura,
-        options: { darkModeSelector: '.p-dark' },
+        options: {darkModeSelector: '.p-dark'},
       },
     }),
-    provideZoneChangeDetection({ eventCoalescing: true }),
-    provideRouter(routes),
+    provideZoneChangeDetection({eventCoalescing: true}),
   ],
 };
