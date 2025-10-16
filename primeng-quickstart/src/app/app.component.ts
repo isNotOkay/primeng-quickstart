c49e3c852fe9caa407444ed5323362b655e2319f
kayod <kay.odenthal@gmx.de>
2025-10-16 09:39:34 +0200

    initial commit

--- primeng-quickstart/src/app/app.component.ts
+++ primeng-quickstart/src/app/app.component.ts
@@ -1,16 +1,40 @@
-import { Component } from '@angular/core';
-import { AppTopbar } from './components/app.topbar';
-import { StatsWidget } from "./components/dashboard/statswidget";
-import { SalesTrendWidget } from "./components/dashboard/salestrendwidget";
-import { RecentActivityWidget } from "./components/dashboard/recentactivitywidget";
-import { ProductOverviewWidget } from "./components/dashboard/productoverviewwidget";
-import { AppFooter } from "./components/app.footer";
+import { Component, OnInit } from '@angular/core';
+import { TableModule } from 'primeng/table';
+import { DecimalPipe } from '@angular/common';
+import { ProductService } from './service/productsservice';
+
+export interface Product {
+  id?: string;
+  code?: string;
+  name?: string;
+  description?: string;
+  price?: number;
+  quantity?: number;
+  inventoryStatus?: string;
+  category?: string;
+  image?: string;
+  rating?: number;
+}
 
 @Component({
   selector: 'app-root',
-  imports: [AppTopbar, StatsWidget, SalesTrendWidget, RecentActivityWidget, ProductOverviewWidget, AppFooter],
+  standalone: true,
+  imports: [TableModule, DecimalPipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  products: Product[] = [];
+  rowsPerPageOptions = [5, 10, 20, 50];
+
+  constructor(private productService: ProductService) {}
+
+  ngOnInit() {
+    // use full dataset so pagination makes sense
+    this.productService.getProducts().then(data => (this.products = data));
+  }
+
+  trackById(index: number, item: Product) {
+    return item.id ?? index;
+  }
 }
